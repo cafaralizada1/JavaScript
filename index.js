088d05fea74ca09e1c1367af2941f83dc61efe2e
cafaralizada1 <sd7eq683b@code.edu.az>
2023-10-22 22:19:13 +0300

    Hello JavaScript

---
+++ index.js
@@ -0,0 +1,146 @@
+// TAPSIRIQ 1
+
+// const a = 2
+
+// const b = 8
+
+// const c = 6
+
+// const diskriminant = b * b - 4 * a * c
+
+// if (diskriminant > 0){
+
+//     const hell1 = (-b + Math.sqrt(diskriminant)) / (2 * a);
+
+//     const hell2 = (-b - Math.sqrt(diskriminant)) / (2 * a);
+
+//     console.log((hell1),(hell2) + "iki hell yolu var");
+
+// }   else if (diskriminant === 0){
+
+//     const hell = -b / (2 * a);
+
+//     console.log((hell) + "bir helli var" );
+
+// }   else {
+
+//     console.log("cavab yoxdur");
+// }
+
+
+
+
+
+// TAPSIRIQ 2
+
+// for (let i = 0; i <= 1000; i++ ){
+//     if ( i % 2 === 0){  
+//         console.log(i);
+//     }
+// }
+
+// TAPSIRIQ 3
+
+// const arr = [2, 4, 5, 9, 1];
+
+// let minElement = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] < minElement) {
+//     minElement = arr[i];
+//   }
+// }
+// console.log(minElement);
+
+
+// TAPSIRIQ 4
+
+// const arr = [2, 4, 5, 9, 1];
+// let maxElement = arr[0];
+
+// for (let i = 1; i < arr.length; i++) {
+//   if (arr[i] > maxElement) {
+//     maxElement = arr[i];
+//   }
+// }
+
+// console.log(maxElement);
+
+// TAPSIRIQ 5
+
+// const arr = [2, 4, 5, 9, 1];
+
+// let minElement = arr[0];
+
+// let minElementIndex = 0;
+
+// for (let i = 1; i < arr.length; i++) {
+
+//   if (arr[i] < minElement) {
+    
+//     minElement = arr[i];
+    
+//     minElementIndex = i;
+//   }
+// }
+// console.log(minElementIndex);
+
+// TAPSIRIQ 6
+
+// const arr = [2, 4, 5, 9, 1];
+
+// let MaxElement = arr[0];
+
+// let MaxElementIndex = 0;
+
+// for (let i = 1; i < arr.length; i++) {
+//     if(arr[i] > MaxElement){
+//         MaxElement = arr[i];
+//         MaxElementIndex = i;
+//     }
+// }
+// console.log(MaxElementIndex);
+
+// TAPSIRIQ 7
+
+// let cem = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (i % 2 === 1) { 
+//     cem += arr[i];
+//   }
+// }
+// console.log(cem);   //4+9=13
+
+// TAPSIRIQ 8
+
+// let i = "Men CodeAcademy Telebesiyem."
+
+// let a = i.toLowerCase();
+
+// console.log(a);
+
+// TAPSIRIQ 9
+
+// let i = "            Mən Code Academydə Programing tədrisi alıram           "
+
+// let a = i.trim();
+
+// console.log(a);
+
+// TAPSIRIQ 10
+
+// let i = "Mən Code Academy Tələbəsiyəm."
+
+// let a = i.split(" ");
+
+// console.log(a);
+
+// TAPSIRIQ 11
+
+// let i = "            Mən Code Academydə Programing tədrisi alıram            ";
+
+// let a = i.replace(/ /g, " ").length;
+
+// console.log(a.length);      //39
+
